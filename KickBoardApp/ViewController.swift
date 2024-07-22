a59d5a42af5f47d410a456abefb51801c3f001fe
Kim-Dong-Hyeon <dhkim960206@gmail.com>
2024-07-22 11:53:20 +0900

    Initial Commit

---
+++ KickBoardApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KickBoardApp
+//
+//  Created by 김동현 on 7/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+

624cdb4e7ca867260a6aa544bd134c393c5bef87
wjstjd120 <wjstjd120@naver.com>
2024-07-22 16:23:09 +0900

    Feat: TabBarController & 하위 Controller 추가

---
+++ KickBoardApp/Controller/EditController.swift
@@ -0,0 +1,17 @@
+//
+//  EditController.swift
+//  KickBoardApp
+//
+//  Created by 김동현 on 7/22/24.
+//
+
+import UIKit
+
+class EditController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
+
--- KickBoardApp/Controller/HomeController.swift
+++ KickBoardApp/Controller/HomeController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeController.swift
 //  KickBoardApp
 //
 //  Created by 김동현 on 7/22/24.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
-
 
 }
 

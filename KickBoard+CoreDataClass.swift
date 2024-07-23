be5c7e406f58c06022b1a3f75d236e277b58df43
Kim-Dong-Hyeon <dhkim960206@gmail.com>
2024-07-23 13:03:15 +0900

    Feat: 지도 화면 구현

---
+++ KickBoard+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  KickBoard+CoreDataClass.swift
+//  KickBoardApp
+//
+//  Created by 김동현 on 7/23/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(KickBoard)
+public class KickBoard: NSManagedObject {
+
+}

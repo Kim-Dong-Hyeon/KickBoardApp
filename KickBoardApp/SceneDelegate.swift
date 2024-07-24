092d4d6fedfd360fcaae24ed791ec2d1e405c194
dbsghdz1 <dbsghdz1@gmail.com>
2024-07-24 11:46:35 +0900

    Feat: 로그아웃기능

--- KickBoardApp/SceneDelegate.swift
+++ KickBoardApp/SceneDelegate.swift
@@ -22,7 +22,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     let window = UIWindow(windowScene: windowScene)
     
     // window 에게 루트 뷰 지정.
-    window.rootViewController = TabBarContorller()
+    let navigationController = UINavigationController(rootViewController: LoginController())
+    window.rootViewController = navigationController
   
     // 이 메서드를 반드시 작성해줘야 윈도우가 활성화 됨.
     window.makeKeyAndVisible()

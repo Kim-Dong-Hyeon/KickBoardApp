1e7ad168d3d5c7496d00c716ba7d72f152290126
dbsghdz1 <dbsghdz1@gmail.com>
2024-07-24 19:36:27 +0900

    Fix: TableView

--- KickBoardApp/Controller/UserProfiles/UsageHistoryController.swift
+++ KickBoardApp/Controller/UserProfiles/UsageHistoryController.swift
@@ -7,22 +7,29 @@
 
 import UIKit
 
+import SnapKit
+
 class UsageHistoryController: UIViewController {
   
-  var usageHistoryView: UsageHistoryView!
-  
-  override func loadView() {
-    usageHistoryView = UsageHistoryView(frame: UIScreen.main.bounds)
-    self.view = usageHistoryView
-  }
+  lazy var usageList = UITableView()
+
   override func viewDidLoad() {
     self.title = "나의 이용 내역"
     view.backgroundColor = .white
     self.navigationController?.navigationBar.prefersLargeTitles = true
     self.navigationItem.largeTitleDisplayMode = .automatic
-    usageHistoryView.usageList.delegate = self
-    usageHistoryView.usageList.dataSource = self
-    usageHistoryView.usageList.register(UsageListCell.self, forCellReuseIdentifier: UsageListCell.identifier)
+    view.addSubview(usageList)
+    setupTableView()
+  }
+  
+  func setupTableView() {
+    usageList.delegate = self
+    usageList.dataSource = self
+    usageList.register(UsageListCell.self, forCellReuseIdentifier: UsageListCell.identifier)
+    usageList.rowHeight = 180
+    usageList.snp.makeConstraints {
+      $0.edges.equalTo(view.safeAreaLayoutGuide).inset(20)
+    }
   }
 }
 
@@ -32,7 +39,7 @@ extension UsageHistoryController: UITableViewDelegate, UITableViewDataSource {
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    guard let cell = usageHistoryView.usageList.dequeueReusableCell(withIdentifier: UsageListCell.identifier, for: indexPath) as? UsageListCell
+    guard let cell = usageList.dequeueReusableCell(withIdentifier: UsageListCell.identifier, for: indexPath) as? UsageListCell
     else { return UITableViewCell() }
     
     return cell
--- KickBoardApp/View/UserProfiles/UsageHistoryView.swift
+++ KickBoardApp/View/UserProfiles/UsageHistoryView.swift
@@ -3,39 +3,5 @@
 //  KickBoardApp
 //
 //  Created by 김윤홍 on 7/22/24.
-//
+// 삭제삭제삭제
 
-import UIKit
-
-import SnapKit
-
-class UsageHistoryView: UIView {
-  
-  lazy var usageList = {
-    let tableView = UITableView()
-    tableView.backgroundColor = .green
-    tableView.rowHeight = 100
-
-    return tableView
-  }()
-  
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-    configureUI()
-    setConstraints()
-  }
-  
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
-  func configureUI() {
-    self.addSubview(usageList)
-  }
-  
-  func setConstraints() {
-    usageList.snp.makeConstraints {
-      $0.edges.equalTo(self.safeAreaLayoutGuide).inset(20)
-    }
-  }
-}

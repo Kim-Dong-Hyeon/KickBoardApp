513b858abcd46691b7ffc920ff4d54025a9067f4
wjstjd120 <wjstjd120@naver.com>
2024-07-26 14:38:37 +0900

    POI 모델 생성

--- KickBoardApp/Model/Model.swift
+++ KickBoardApp/Model/Model.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+import CoreLocation
+
+struct PoiData {
+    var id: String
+    var position: CLLocationCoordinate2D
+    var styleID: String
+    var name: String
+}
+
+// dummy data 생성
+class PoiDataSample {
+    static func createPoiData() -> [PoiData] {
+        if datas.isEmpty == false {
+            return datas
+        }
+        
+        let center = CLLocationCoordinate2D(latitude: 37.533, longitude: 126.996)
+        
+        let styleIds = [
+            "orange",
+            "red",
+            "green",
+            "blue"
+        ]
+        
+        for index in 0 ... 100 {
+            let position = CLLocationCoordinate2D(latitude: center.latitude + drand48(),
+                                                  longitude: center.longitude + drand48())
+            let data = PoiData(id: "poi" + String(index),
+                               position: position,
+                               styleID: "label_default_style",
+                               name: "place" + String(index))
+            
+            datas.append(data)
+        }
+        
+        return datas
+    }
+
+    static var datas = [PoiData]()
+}
